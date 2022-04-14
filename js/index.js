97cfa6e2454baa2f17711e9abc8320c9a7575b1d
jsore1 <m79054050200@gmail.com>
2022-04-14 16:58:26 +0300

    Добавлена загрузка информации по объекту

--- js/index.js
+++ js/index.js
@@ -3,6 +3,34 @@ const clientHeight = 557;
 const rightArray = [335, 407, 468, 545, 583, 277, 310, 355, 398, 480, 545, 600];
 
 const pointArray = document.querySelectorAll(".plan__point");
+const imageWindowImg = document.querySelector(".image-window").children[0];
+const imageWindowTitle = document.querySelector(".image-window__title");
+const imageWindowDate = document.querySelector(".image-window__date");
+
+const requestUrl = `https://jsore-games.ru/imagesWindow.json`;
+
+let obj;
+
+let request = new XMLHttpRequest();
+request.open('GET', requestUrl);
+request.responseType = 'json';
+request.send();
+request.onload = () => {
+    obj = request.response;
+    console.log(obj);
+}
+
+pointArray.forEach((element, index) => {
+    element.addEventListener("mouseenter", () => {
+        obj.imagesWindow.forEach((el) => {
+            if (el.id === index) {
+                imageWindowImg.src = el.urlImage;
+                imageWindowTitle.innerText = el.title;
+                imageWindowDate.innerText = el.date;
+            }
+        });
+    });
+});
 
 window.addEventListener("resize", () => {
     pointArray.forEach((element, index) => {
