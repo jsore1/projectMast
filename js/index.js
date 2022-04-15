2f8d4df096bbe6ab32d5fe813c4b6fa98e864e1c
jsore1 <m79054050200@gmail.com>
2022-04-15 10:27:55 +0300

    Добавил заполнение таблицы

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const pointArray = document.querySelectorAll(".plan__point");
 const imageWindowImg = document.querySelector(".image-window").children[0];
 const imageWindowTitle = document.querySelector(".image-window__title");
 const imageWindowDate = document.querySelector(".image-window__date");
+const tbody = document.querySelector(".main-table").children[0].children[1];
+const reportWindowPanel = document.querySelector(".report-window__panel").children[1];
 
 const requestUrl = `https://jsore-games.ru/imagesWindow.json`;
 
@@ -20,12 +22,29 @@ request.onload = () => {
 }
 
 let requestTable = new XMLHttpRequest();
-request.open('GET', `https://jsore-games.ru/table.json`);
-request.responseType = 'json';
-request.send();
-request.onload = () => {
-    const obj = request.response;
-    //Продолжить заполнение таблицы
+requestTable.open('GET', `https://jsore-games.ru/table.json`);
+requestTable.responseType = 'json';
+requestTable.send();
+requestTable.onload = () => {
+    const obj = requestTable.response;
+
+    obj.table.forEach((el, index) => {
+        const report = (el.report) ? `<a href="#report-window">Открыть</a>` : ``;
+        const tr = `<tr>
+                        <td>${el.date}</td>
+                        <td>${el.numberObj}</td>
+                        <td>${el.descr}</td>
+                        <td>${el.secondName}</td>
+                        <td>${report}</td>
+                    </tr>`;
+        tbody.insertAdjacentHTML('beforeend', tr);
+
+        if (el.report) {
+            tbody.children[index].children[4].children[0].addEventListener("mouseenter", () => {
+                reportWindowPanel.innerText = el.reportText;
+            });
+        }
+    });
 }
 
 pointArray.forEach((element, index) => {
