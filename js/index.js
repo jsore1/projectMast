d70b809a1ffa73878f7cb6d87dddc099ce39f03d
jsore1 <m79054050200@gmail.com>
2022-06-02 14:36:50 +0300

    Изменил код для модальных окон

--- js/index.js
+++ js/index.js
@@ -1,12 +1,54 @@
+class Modal {
+    constructor() {
+        this.modal = document.createElement('div');
+        document.body.append(this.modal);
+        this.modal.classList.add('modal');
+        this.dialog = document.createElement('div');
+        this.modal.append(this.dialog);
+        this.dialog.classList.add('modal__dialog');
+        this.content = document.createElement('div');
+        this.dialog.append(this.content);
+        this.content.classList.add('modal__content');
+        this.close = document.createElement('div');
+        this.content.append(this.close);
+        this.close.dataset.close = true;
+        this.close.classList.add('modal__close');
+        this.close.innerHTML = '&times;';
+        this.title = document.createElement('div');
+        this.content.append(this.title);
+        this.title.classList.add('modal__title');
+    }
+    
+    createReportDialog() {
+        if (this.image) {this.image.remove();}
+        if (this.date) {this.date.remove();}
+        if (this.text) {this.text.remove();}
+        this.text = document.createElement('div');
+        this.content.append(this.text);
+        this.text.classList.add('modal__text');
+    }
+
+    createImageDialog() {
+        if (this.text) {this.text.remove();}
+        if (this.image) {this.image.remove();}
+        if (this.date) {this.date.remove();}
+        this.image = document.createElement('div');
+        this.image.classList.add('modal__img');
+        this.content.prepend(this.image);
+        this.date = document.createElement('div');
+        this.content.append(this.date);
+        this.date.classList.add('modal__date');
+    }
+}
 let isAddPoint = false;
 let isUpdatePoint = false;
 
-const imageWindowImg = document.querySelector(".image-window").children[0];
-const imageWindowTitle = document.querySelector(".image-window__title");
-const imageWindowDate = document.querySelector(".image-window__date");
-const tbody = document.querySelector(".main-table").children[0].children[1];
-const reportWindowPanel = document.querySelector(".report-window__panel").children[1];
-const plan = document.querySelector(".plan");
+const modalDialog = new Modal();
+
+const tbody = document.querySelector(".main-table").children[0].children[1],
+    plan = document.querySelector(".plan");
+let modalTrigger = document.querySelectorAll('[data-modal]');
+let modalTriggerImg = document.querySelectorAll('[data-modal]');
 
 let requestSelect = new XMLHttpRequest();
 requestSelect.open('POST', 'select_points.php');
@@ -16,16 +58,26 @@ requestSelect.onload = () => {
     const array = requestSelect.response;
     if (array.length > 0) {
         array.forEach((el) => {
-            plan.insertAdjacentHTML("beforeend",`<a href="#image-window"><div class="plan__point" style="top: ${el.y}px;left: ${el.x}px"><span>${el.name}</span></div></a>`);
+            plan.insertAdjacentHTML(
+                "beforeend",
+                `<a href="#" data-modal>
+                    <div class="plan__point" style="top: ${el.y}px;left: ${el.x}px">
+                        <span>${el.name}</span>
+                    </div>
+                </a>`
+            );
         });
         const pointArray = document.querySelectorAll(".plan__point");
         pointArray.forEach((element, index) => {
             element.addEventListener("mouseenter", () => {
+                modalDialog.createImageDialog();
                 array.forEach((el) => {
                     if ((el.id - 1) === index) {
-                        imageWindowImg.src = el.url;
-                        imageWindowTitle.innerText = el.title;
-                        imageWindowDate.innerText = el.date;
+                        //modalDialog.image.src = `${el.url}`;
+                        modalDialog.image.style.background = `url(${el.url}) center center/cover no-repeat`;
+                        console.log("text");
+                        modalDialog.title.textContent = el.title;
+                        modalDialog.date.textContent = el.date;
                     }
                 });
             });
@@ -50,13 +102,21 @@ requestSelect.onload = () => {
                             if (request.response === "1") {
                                 console.log("Точка обновлена!");
                             }
-                        }
+                        };
                     }
                 }
             });
         });
+        modalTriggerImg = document.querySelectorAll('[data-modal]');
+
+        modalTriggerImg.forEach(btn => {
+            btn.addEventListener('click', function() {
+                modalDialog.modal.classList.add('show');
+                modalDialog.modal.classList.remove('hide');
+            });
+        });
     }
-}
+};
 
 
 plan.addEventListener("click", (event) => {
@@ -83,24 +143,19 @@ plan.addEventListener("click", (event) => {
             request.onload = () => {
                 if (request.response === "1") {
                     console.log("Точка добавлена!");
-                    plan.insertAdjacentHTML("beforeend",`<a href="#image-window"><div class="plan__point" style="top: ${top}px;left: ${left}px"><span>${pointName}</span></div></a>`);
+                    plan.insertAdjacentHTML(
+                        "beforeend",
+                        `<a href="#" data-modal>
+                            <div class="plan__point" style="top: ${top}px;left: ${left}px">
+                                <span>${pointName}</span>
+                            </div>
+                        </a>`
+                    );
                 }
-            }
+            };
         }
     }
 });
-
-//const requestUrl = `https://jsore-games.ru/imagesWindow.json`;
-
-//let obj;
-
-//let request = new XMLHttpRequest();
-//request.open('GET', requestUrl);
-//request.responseType = 'json';
-//request.send();
-//request.onload = () => {
-    //obj = request.response;
-//}
 
 let requestTable = new XMLHttpRequest();
 requestTable.open('GET', `table.json`);
@@ -110,7 +165,7 @@ requestTable.onload = () => {
     const obj = requestTable.response;
 
     obj.table.forEach((el, index) => {
-        const report = (el.report) ? `<a href="#report-window">Открыть</a>` : ``;
+        const report = (el.report) ? `<a href="#" data-modal>Открыть</a>` : ``;
         const tr = `<tr>
                         <td>${el.date}</td>
                         <td>${el.numberObj}</td>
@@ -122,11 +177,35 @@ requestTable.onload = () => {
 
         if (el.report) {
             tbody.children[index].children[4].children[0].addEventListener("mouseenter", () => {
-                reportWindowPanel.innerText = el.reportText;
+                modalDialog.createReportDialog();
+                modalDialog.title.textContent = 'Отчет';
+                modalDialog.text.textContent = el.reportText;
             });
         }
     });
+    modalTrigger = document.querySelectorAll('[data-modal]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', function() {
+            modalDialog.modal.classList.add('show');
+            modalDialog.modal.classList.remove('hide');
+        });
+    });
+};
+
+// Функция, которая закрывает модальное окно
+function closeModal() {
+    modalDialog.modal.classList.add('hide');
+    modalDialog.modal.classList.remove('show');
 }
+
+modalDialog.close.addEventListener('click', closeModal);
+
+modalDialog.modal.addEventListener('click', (e) => {
+    if (e.target === modalDialog.modal) {
+        closeModal();
+    }
+});
 
 function addPointToggle() {
     isAddPoint = (isAddPoint) ? false : true;
