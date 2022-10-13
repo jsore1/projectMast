f7724384d669a09198aed7a34678560e6f7f1080
jsore <m79054050200@gmail.com>
2022-10-13 10:41:51 +0300

    изменил пароль

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,6 @@
 <?php
 $db_name = 'projectmast_db';
 $login = 'root';
-$pass = 'HefylfIfh48';
+$pass = '';
 $db = new PDO('mysql:host=localhost;dbname=' . $db_name .';charset=utf8;', $login, $pass);
 ?>
