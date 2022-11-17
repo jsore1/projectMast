dd8c6a926453ca4e1b9ded056bd1188a5ac813bb
jsore <m79054050200@gmail.com>
2022-11-17 18:52:20 +0300

    Теперь выборка из архива последних двух фото

--- select_images_archive.php
+++ select_images_archive.php
@@ -1,7 +1,7 @@
 <?php
 require_once('connect_db.php');
 
-$sql = "SELECT * FROM `img_archive` WHERE `point_id` = :id";
+$sql = "SELECT * FROM `img_archive` WHERE `point_id` = :id ORDER BY `id` DESC LIMIT 2";
 $stmt = $db->prepare($sql);
 $stmt->execute([':id' => $_POST['id']]);
 $array = array();
